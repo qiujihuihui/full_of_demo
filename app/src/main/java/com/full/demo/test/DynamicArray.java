0840a463f41b101f6ff59349efdafb91f88dfa81
huihui <1468901064@qq.com>
2019-12-14 20:50:12 +0800

    新增XUtils框架

---
+++ app/src/main/java/com/full/demo/test/DynamicArray.java
@@ -0,0 +1,26 @@
+package com.full.demo.test;
+
+public class DynamicArray {
+    private static final int DEFAULT_CAPACITY = 10;
+    protected int size = 0;
+    protected int capacity = DEFAULT_CAPACITY;
+    protected Integer[] elements = new Integer[DEFAULT_CAPACITY];
+
+    public int size() {
+        return this.size;
+    }
+
+    public Integer get(int index) {
+        return elements[index];
+    }
+
+    public void add(Integer e) {
+        System.out.println("父类：" + e);
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    protected void ensureCapacity() {
+        //...如果数组满了就扩容...代码省略...
+    }
+}
