cc870374c2f209d4ee02abbffe01212af96b83e3
qiujihuihui <1468901064@qq.com>
2020-05-12 22:48:14 +0800

    Animator属性动画

--- app/src/main/java/com/full/demo/businessdemo/customview/FirstViewActivity.java
+++ app/src/main/java/com/full/demo/businessdemo/customview/FirstViewActivity.java
@@ -1,6 +1,10 @@
 package com.full.demo.businessdemo.customview;
 
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +34,47 @@ public class FirstViewActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        customView.smoothScrollTo(-400, -400, 5000);
+//        customView.smoothScrollTo(-400, -400, 5000);
+        startShakeByPropertyAnim(customView, 0.5f, 0.8f, 5.0f, 2000);
+    }
+
+    private void startShakeByPropertyAnim(View view, float scaleSmall, float scaleLarge, float shakeDegrees, long duration){
+        if (view == null) return;
+        //TODO 验证参数的有效性
+
+        //先变小后变大
+        PropertyValuesHolder scaleXValuesHolder = PropertyValuesHolder.ofKeyframe(View.SCALE_X,
+                Keyframe.ofFloat(0f, 1.0f),
+                Keyframe.ofFloat(0.25f, scaleSmall),
+                Keyframe.ofFloat(0.5f, scaleLarge),
+                Keyframe.ofFloat(0.75f, scaleLarge),
+                Keyframe.ofFloat(1.0f, 1.0f)
+        );
+        PropertyValuesHolder scaleYValuesHolder = PropertyValuesHolder.ofKeyframe(View.SCALE_Y,
+                Keyframe.ofFloat(0f, 1.0f),
+                Keyframe.ofFloat(0.25f, scaleSmall),
+                Keyframe.ofFloat(0.5f, scaleLarge),
+                Keyframe.ofFloat(0.75f, scaleLarge),
+                Keyframe.ofFloat(1.0f, 1.0f)
+        );
+
+        //先往左再往右
+        PropertyValuesHolder rotateValuesHolder = PropertyValuesHolder.ofKeyframe(View.ROTATION,
+                Keyframe.ofFloat(0f, 0f),
+                Keyframe.ofFloat(0.1f, -shakeDegrees),
+                Keyframe.ofFloat(0.2f, shakeDegrees),
+                Keyframe.ofFloat(0.3f, -shakeDegrees),
+                Keyframe.ofFloat(0.4f, shakeDegrees),
+                Keyframe.ofFloat(0.5f, -shakeDegrees),
+                Keyframe.ofFloat(0.6f, shakeDegrees),
+                Keyframe.ofFloat(0.7f, -shakeDegrees),
+                Keyframe.ofFloat(0.8f, shakeDegrees),
+                Keyframe.ofFloat(0.9f, -shakeDegrees),
+                Keyframe.ofFloat(1.0f, 0f)
+        );
+
+        ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(view, scaleXValuesHolder, scaleYValuesHolder, rotateValuesHolder);
+        objectAnimator.setDuration(duration);
+        objectAnimator.start();
     }
 }
